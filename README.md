2c14610ae2b231554cc34e3d99d5c8e95b767a40
Apostolos P. Tsompanopoulos <monotropos@gmail.com>
2023-07-09 19:51:11 +0300

    First release 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# liketoday
+# LikeToday
+Contributors: @monotropos
+Tags: posts list
+Tested up to: 6.2.2
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+
+## Description
+
 Show posts (titles and URLs) from WordPress blogs posted some years ago on today's date.
+
+The plugin does not collect any personal data, so it is **ready for EU General Data Protection Regulation (GDPR) compliance**.
+
+### Lightweight, simple and effective
+
+No huge widget with hundreds of options, just one.
+
+### Options you can set
+
+1. Title of the widget
+
+### Languages
+
+Just English for the time being.
+Admins can define widget's title to whatever they like (every supported language) through the Widgets setup screen, as seen in screenshot-1.png
+
+
+## Installation
+
+### Using The WordPress Dashboard
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'LikeToday'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+5. Go to 'Appearance' => 'Widgets' and select 'LikeToday'
+
+
+## Frequently Asked Questions
+
+It's the initial release, none yet.
+
+## Changelog
+
+### 0.0.1
+* Initial release
---
+++ liketoday.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: LikeToday
+Version: 0.0.1
+Plugin URI: https://github.com/monotropos/liketoday
+Description: Show posts (titles and URLs) posted some years ago on today's date.
+Author: Apostolos P. Tsompanopoulos
+Author URI: https://www.aptlogs.com/
+*/
+
+/* Version check */
+function liketoday_url( $path = '' ) {
+	global $wp_version;
+	if ( version_compare( $wp_version, '2.8', '<' ) ) { // Using WordPress 2.7
+		$folder = dirname( plugin_basename( __FILE__ ) );
+		if ( '.' != $folder ) $path = path_join( ltrim( $folder, '/' ), $path );
+		return plugins_url( $path );
+	}
+	return plugins_url( $path, __FILE__ );
+}
+
+// Create the widget
+class liketoday_widget extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+			'liketoday_widget',									// Base ID of your widget
+			__('LikeToday Widget', 'liketoday_widget_domain'),	// Widget name will appear in UI
+			array( 'description' => __( 'Simple widget to show posts from today’s date', 'liketoday_widget_domain' ), ) // Widget description
+		);
+	}
+
+	// Creating widget front-end
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+
+		$tmon = date("m");
+		$tday = date("d");
+		$query = new WP_Query( "monthnum=$tmon&day=$tday");
+
+		// The Loop.
+		if ( $query->have_posts() ) {
+			$msg = '<ul>';
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$msg .= '<li><a href="'. get_the_permalink() .'">' . esc_html( get_the_title() ) . '</a><br/>'. get_the_date() .'</li>';
+			}
+			$msg .= '</ul>';
+		} else {
+			$msg = 'Sorry, no posts posted on a date like today.';
+		}
+		echo __( $msg, 'liketoday_widget_domain' );
+		// Restore original Post Data.
+		wp_reset_postdata();
+
+		echo $args['after_widget'];
+	}
+
+	// Widget back-end
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title for widget', 'liketoday_widget_domain' );
+		}
+		// Widget admin form
+		?>
+<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+	<input id="<?php echo $this->get_field_id( 'title' ) ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php
+	}
+
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+
+	// Class liketoday_widget ends here
+}
+
+// Register and load the widget
+function liketoday_load_widget() {
+	register_widget( 'liketoday_widget' );
+}
+add_action( 'widgets_init', 'liketoday_load_widget' );
+
+?>
